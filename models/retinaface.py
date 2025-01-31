8b3044fb8e1a845b4f5a670b4e6736dd50e382ed
codewarrior26 <sumedhhp8@gmail.com>
2025-01-31 10:32:55 -0600

    Update retinaface.py

--- models/retinaface.py
+++ models/retinaface.py
@@ -110,7 +110,7 @@ class RetinaFace(nn.Module):
 
     def forward(self,inputs):
         out = self.body(inputs)
-        print("body output type ", out.shape)
+        print("body output type ", type(out))
         print("len of body output ", len(out))
         
 
